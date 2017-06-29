a38af1a140c79616d0334e615f8a17b285dd1ee2
Himanshu Mittal <mittal01091997@gmail.com>
2017-06-29 18:38:21 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+// const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
+});
+
+module.exports = {app};
